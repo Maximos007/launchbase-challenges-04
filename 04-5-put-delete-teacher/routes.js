642e680f7747fba0d4f7c5c4abb03b3954c49b91
Maximos007 <waslleymax@gmail.com>
2021-05-17 14:49:24 -0300

    challenge 4-5 done

---
+++ 04-5-put-delete-teacher/routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const teachers = require('./teachers');
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+	return res.redirect('/teachers');
+});
+
+routes.get('/teachers', (req, res) => {
+	return res.render('teachers/index');
+});
+
+routes.get('/teachers/create', (req, res) => {
+	return res.render('teachers/create');
+});
+
+routes.get('/teachers/:id', teachers.show);
+
+routes.get('/teachers/:id/edit', teachers.update);
+
+routes.post('/teachers', teachers.create); // Formulário 
+
+routes.put('/teachers', teachers.put);
+
+routes.delete('/teachers', teachers.delete);
+
+routes.get('/students', (req, res) => {
+	return res.send('alunos');
+});
+
+module.exports = routes;
---
+++ 04-5-put-delete-teacher/teachers.js
@@ -0,0 +1,127 @@
+const fs = require('fs');
+const data = require('./data.json');
+// eslint-disable-next-line no-unused-vars
+const { age, graduation, date } = require('./utils');
+
+module.exports = {
+  
+	create(req, res) {
+		const keys = Object.keys(req.body);
+
+		// eslint-disable-next-line no-undef
+		for (key of keys) {
+			// eslint-disable-next-line no-undef
+			if(req.body[key] == '')
+      
+				return res.send('Please, fill all fields!');
+		}
+    
+		let { avatar_url, name, birth, education, class_type, services } = req.body;
+
+		birth = Date.parse(birth);
+		const created_at = Date.now();
+		const id = Number(data.teachers.length + 1);
+
+		data.teachers.push({
+			id,
+			avatar_url,
+			name,
+			birth,
+			education,
+			class_type,
+			services,
+			created_at,
+		});
+
+		fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+			if (err) return res.send('Write file error!');
+
+			return res.send('was successfully created');
+		});
+
+	},
+
+	show(req, res) {
+
+		const { id } = req.params;
+
+		const foundTeacher = data.teachers.find(teacher => {
+			return teacher.id == id;
+		});
+
+		if(!foundTeacher) return res.send('Teacher not found!');
+
+		const teachers = {
+			...foundTeacher,
+			services: foundTeacher.services.split(','),
+			age: age(foundTeacher.birth),
+			created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeacher.created_at),
+			education: graduation(foundTeacher.education),
+		};
+
+		return res.render('teachers/show', { teachers });
+	},
+
+	update(req, res) {
+
+		const { id } = req.params;
+
+		const foundTeacher = data.teachers.find(teacher => {
+			return teacher.id == id;
+		});
+
+		if(!foundTeacher) return res.send('Teacher not found!');
+
+		const teachers = {
+			...foundTeacher,
+			birth: date(foundTeacher.birth),
+		};
+
+		return res.render('teachers/edit', { teachers });
+	},
+
+	put(req, res) {
+		const { id } = req.body;
+		let index = 0;
+
+		const foundTeacher = data.teachers.find((teacher, foundIndex) => {
+			if(teacher.id == id) {
+				index = foundIndex;
+
+				return true;
+			}
+		});
+
+		if(!foundTeacher) return res.send('Teacher not found!');
+
+		const teachers = { 
+			...foundTeacher,
+			...req.body,
+			birth: Date.parse(req.body.birth)
+		};
+
+		data.teachers[index] = teachers;
+
+		fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+			if(err) return res.send('Write error!');
+
+			return res.redirect(`/teachers/${id}`);
+		});
+	},
+
+	delete(req, res) {
+		const { id } = req.body;
+
+		const filteredTeachers = data.teachers.filter((teachers) => {
+			return teachers.id != id;
+		});
+
+		data.teachers = filteredTeachers;
+
+		fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+			if (err) return res.send('Write error!');
+
+			return res.send('successfully deleted');
+		});
+	}
+};
