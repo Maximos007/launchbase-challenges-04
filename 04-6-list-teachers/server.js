8b838df6da5896f7e014d2a1c97117249f6b0c6b
Maximos007 <waslleymax@gmail.com>
2021-05-17 17:39:21 -0300

    challege 4-6 done

---
+++ 04-6-list-teachers/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const methodOverride = require('method-override');
+
+const server = express();
+
+server.use(express.static(__dirname + '/public'));
+server.use(express.urlencoded({ extended: true }));
+server.use(methodOverride('_method'));
+server.use(routes);
+
+server.set('view engine', 'html');
+
+nunjucks.configure('views', {
+	express:server,
+	autoescape: false,
+	noCache: true
+});
+
+server.listen(3335);
+
